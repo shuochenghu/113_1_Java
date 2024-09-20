8bf568dd988bf36c5b4813bc4d3cc722f150f960
shuochenghu <shuochenghu@gmail.com>
2024-09-20 12:01:21 +0800

    113-09-20

--- 113-09-20/Range.java
+++ 113-09-20/Range.java
@@ -1,8 +1,7 @@
 public class Range {
     public static void main(String[] args) {
         // byte、short、int、long 範圍
-        System.out.printf("%d ~ %d%n", 
-                Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("%d ~ %d%n", Byte.MIN_VALUE, Byte.MAX_VALUE);
         System.out.printf("%d ~ %d%n", 
                 Short.MIN_VALUE, Short.MAX_VALUE);
         System.out.printf("%d ~ %d%n", 
