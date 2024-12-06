5d678e43475e33e69ce2a3cc1d6ea3484e9fd6e2
shuochenghu <shuochenghu@gmail.com>
2024-12-06 12:01:55 +0800

    113-12-06

--- 113-11-29/InnerClass/src/CalculatorTest.java
+++ 113-11-29/InnerClass/src/CalculatorTest.java
@@ -1,6 +1,7 @@
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
         calculator.performOperations(10, "+", 5);
         calculator.performOperations(10, "-", 5);
         calculator.performOperations(10, "*", 5);
---
+++ 113-12-06/TryCatch/src/CheckedException.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        try {
+            File file = new File("nonexistentfile.txt");
+            FileReader reader = new FileReader(file); // 可能拋出 FileNotFoundException
+        } catch (FileNotFoundException e) {
+            System.out.println("處理受檢例外: " + e.getMessage());
+        }
+    }
+}
